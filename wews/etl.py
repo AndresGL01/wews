90b3f57c92b6edf902532076e7690d2ed9e54bb2
Winters <andreslp0001@gmail.com>
2024-01-16 18:50:40 +0100

    Dockerization

--- wews/etl.py
+++ wews/etl.py
@@ -6,6 +6,7 @@ import polars
 import polars as pl
 
 from airflow.decorators import dag, task
+import tldextract
 
 from sources.base import UrlHaus, Kaggle
 from loader.base import Drive, Client
@@ -23,6 +24,9 @@ from google.oauth2 import service_account
 def etl():
     @task()
     def authenticate() -> Client:
+        """
+        Authenticate to the corresponding services
+        """
         scopes = ['https://www.googleapis.com/auth/drive']
         service_account_file = '/home/winters/apps/wews/wews/wews-402510-7ad9cc194bdf.json'
 
@@ -40,6 +44,9 @@ def etl():
 
     @task()
     def extract(static_data: list, client: Client) -> polars.DataFrame:
+        """
+        Extract the dynamic data, mix the dataframes and push it to the data lake
+        """
         kaggle_spec = [file for file in static_data if file.get('id') == '1B4_yhwswIM22R2haNQKMV-_lUZg4u51h'][0]
         client.get_resource(resource_id=kaggle_spec.get('id'), resource_name=kaggle_spec.get('name'))
 
@@ -61,6 +68,16 @@ def etl():
 
     @task()
     def transform(bronze_dataframe: polars.DataFrame) -> polars.DataFrame:
+        """
+        Clean columns and create new ones
+        """
+
+        def treat(value: str) -> str:
+            """
+            Return the url domain
+            """
+            return tldextract.extract(value).domain
+
         predicate = pl.when(pl.col('threat').is_not_null()).then(pl.col('threat')).otherwise(pl.col('type'))
 
         silver_dataframe = bronze_dataframe.drop(
@@ -68,10 +85,17 @@ def etl():
             predicate.alias('threat_type')
         ).drop(['type', 'threat'])
 
+        silver_dataframe = silver_dataframe.with_columns(
+            pl.col('url').map_elements(treat).alias('domain')
+        )
+
         return silver_dataframe
 
     @task()
     def load(silver_dataframe: polars.DataFrame, drive_client: Client) -> None:
+        """
+        Takes the silver dataframe and push it to the data lake
+        """
         filename = f'{datetime.now().strftime("%Y_%m_%d")}_gold.parquet'
         target_path = f'{os.getenv("TEMPORAL_STATIC_DATA_PATH")}{filename}'
         silver_dataframe.write_parquet(target_path)
@@ -81,6 +105,9 @@ def etl():
         cleanup()
 
     def cleanup() -> None:
+        """
+        Remove all static data/auxiliar files in tmp folder
+        """
         shutil.rmtree(os.getenv("TEMPORAL_STATIC_DATA_PATH"))
 
     drive = authenticate()
--- wews/main.py
+++ wews/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from datetime import datetime
 
 from dotenv import load_dotenv
 
@@ -7,20 +8,56 @@ from google.oauth2 import service_account
 import polars as pl
 
 from wews.loader.base import Drive
+from wews.sources.base import Kaggle, UrlHaus
 
+import tldextract
 
 load_dotenv()
 
+
+def treat(value: str):
+    return tldextract.extract(value).domain
+
+
 if __name__ == '__main__':
-    shutil.rmtree(os.getenv("TEMPORAL_STATIC_DATA_PATH"))
-    # SCOPES = ['https://www.googleapis.com/auth/drive']
-    # SERVICE_ACCOUNT_FILE = '/home/winters/apps/wews/wews/wews-402510-7ad9cc194bdf.json'
-    #
-    # credentials = service_account.Credentials.from_service_account_file(
-    #     SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-    #
-    # drive = Drive(credentials=credentials)
-    # static_data = drive.retrieve_static_data()
+    scopes = ['https://www.googleapis.com/auth/drive']
+    service_account_file = '/home/winters/apps/wews/wews/wews-402510-7ad9cc194bdf.json'
+
+    credentials = service_account.Credentials.from_service_account_file(
+        service_account_file, scopes=scopes)
+
+    drive = Drive(credentials=credentials)
+    static = drive.retrieve_static_data()
+
+    kaggle_spec = [file for file in static if file.get('id') == '1B4_yhwswIM22R2haNQKMV-_lUZg4u51h'][0]
+    drive.get_resource(resource_id=kaggle_spec.get('id'), resource_name=kaggle_spec.get('name'))
+
+    dataframes = [
+        UrlHaus().get(),
+        Kaggle().get(
+            file_spec=kaggle_spec,
+        )
+    ]
+    mix = pl.concat(dataframes, how="diagonal")
+
+    filename = f'{datetime.now().strftime("%Y_%m_%d")}_bronze.parquet'
+    target_path = f'{os.getenv("TEMPORAL_STATIC_DATA_PATH")}{filename}'
+    mix.write_parquet(target_path)
+
+    drive.push_file(target_path, os.getenv('WAREHOUSE_BRONZE_FOLDER_ID'), filename)
+
+    predicate = pl.when(pl.col('threat').is_not_null()).then(pl.col('threat')).otherwise(pl.col('type'))
+
+    silver_dataframe = mix.drop(
+        ['last_online', 'tags', 'id', 'urlhaus_link', 'url_status', 'dateadded', 'reporter']).with_columns(
+        predicate.alias('threat_type')
+    ).drop(['type', 'threat'])
+
+    silver_dataframe = silver_dataframe.with_columns(
+        pl.col('url').map_elements(treat).alias('domain')
+    )
+
+    print(silver_dataframe)
 
     # dataframes = []
     # for file in static_data:
